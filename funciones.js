fc34532f713274052b2728f6af52b5af1a3c7754
Tomber <tomuriel01.07@gmail.com>
2025-10-21 09:30:57 -0300

    Detalles y Arreglos 1

--- funciones.js
+++ funciones.js
@@ -1,1 +1,14 @@
 const formulariocontacto = document.getElementById("form-contacto");
+
+formulariocontacto.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const confirmar = prompt("¿Deseas enviar el mensaje? (sí / no)");
+
+  if (confirmar && confirmar.toLowerCase() === "si") {
+    alert("✅ ¡Mensaje enviado con éxito!");
+    formulariocontacto.reset();
+  } else {
+    alert("❌ Envío cancelado");
+  }
+});
